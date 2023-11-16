2b02e1e4664c86ed658a43a7e2d744182001f8a8
GalaxJ6 <sfernandezga@unal.edu.co>
2023-11-15 23:28:51 -0500

    Se añade el middleware para los roles

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@ Route::get('/', function(){
 });
 
 
-// Route::get('/', [AdministratorsController::class,'index'])-> name('authors.index');
-// Route::post('/authors', [AdministratorsController::class, 'store'])->name('authors.store');
-// Route::delete('/authors/{id}', [AdministratorsController::class, 'destroy'])->name('authors.destroy');
-// Route::put('/authors/{id}', [AdministratorsController::class, 'update'])->name('authors.update');
-// Route::get('/authors_edit/{id}', [AdministratorsController::class, 'edit'])->name('authors.edit');
+Route::get('/', [UsersController::class,'index'])-> name('authors.index');
+Route::post('/authors', [UsersController::class, 'store'])->name('authors.store');
+Route::delete('/authors/{id}', [UsersController::class, 'destroy'])->name('authors.destroy');
+Route::put('/authors/{id}', [UsersController::class, 'update'])->name('authors.update');
+Route::get('/authors_edit/{id}', [UsersController::class, 'edit'])->name('authors.edit');
 
 
 // Route::get('/books', [CampusController::class, 'index'])->name('books.index');
