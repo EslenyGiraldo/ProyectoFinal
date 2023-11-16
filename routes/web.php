458a260a303e0735b6384aa8dc57846527d1e172
GalaxJ6 <sfernandezga@unal.edu.co>
2023-11-15 23:35:19 -0500

    Se actualiza el archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,13 @@ Route::get('/', function(){
 });
 
 
-Route::get('/', [UsersController::class,'index'])-> name('authors.index');
-Route::post('/authors', [UsersController::class, 'store'])->name('authors.store');
-Route::delete('/authors/{id}', [UsersController::class, 'destroy'])->name('authors.destroy');
-Route::put('/authors/{id}', [UsersController::class, 'update'])->name('authors.update');
-Route::get('/authors_edit/{id}', [UsersController::class, 'edit'])->name('authors.edit');
+Route::get('/', [UsersController::class,'index'])-> name('users.index');
+Route::post('/users', [UsersController::class, 'store'])->name('users.store');
+Route::delete('/users/{id}', [UsersController::class, 'destroy'])->name('users.destroy');
+Route::put('/users/{id}', [UsersController::class, 'update'])->name('users.update');
+Route::get('/users_edit/{id}', [UsersController::class, 'edit'])->name('users.edit');
 
 
-// Route::get('/books', [CampusController::class, 'index'])->name('books.index');
-// Route::post('/books', [CampusController::class, 'store'])->name('books.store');
-// Route::delete('/books/{id}', [CampusController::class, 'destroy'])->name('books.destroy');
-// Route::put('/books/{id}', [CampusController::class, 'update'])->name('books.update');
-// Route::get('/books_edit/{id}', [CampusController::class, 'edit'])->name('books.edit');
-
-// Route::get('/', [SportsController::class,'index'])-> name('authors.index');
-// Route::post('/authors', [SportsController::class, 'store'])->name('authors.store');
-// Route::delete('/authors/{id}', [SportsController::class, 'destroy'])->name('authors.destroy');
-// Route::put('/authors/{id}', [SportsController::class, 'update'])->name('authors.update');
-// Route::get('/authors_edit/{id}', [SportsController::class, 'edit'])->name('authors.edit');
-
-
-// Route::get('/books', [UsersController::class, 'index'])->name('books.index');
-// Route::post('/books', [UsersController::class, 'store'])->name('books.store');
-// Route::delete('/books/{id}', [UsersController::class, 'destroy'])->name('books.destroy');
-// Route::put('/books/{id}', [UsersController::class, 'update'])->name('books.update');
-// Route::get('/books_edit/{id}', [UsersController::class, 'edit'])->name('books.edit');
+Route::get('/administrators', [AdministratorsController::class, 'index'])->name('administrators.index');
+Route::put('/administrators/{id}', [AdministratorsController::class, 'update'])->name('administrators.update');
+Route::get('/administrators_edit/{id}', [AdministratorsController::class, 'edit'])->name('administrators.edit');
