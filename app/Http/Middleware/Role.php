f2ecdc762b297e7264bb198cea2cf99278d65bbf
EslenyGiraldo <esgiraldov@unal.edu.co>
2023-11-28 22:45:40 -0500

    middleware

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Role
+{
+ 
+    public function handle($request, Closure $next, $roles)
+    {
+        $newRole=explode('|',$roles);
+        $roleName=strtolower($request->user()->role->label);
+        if(in_array($roleName,$newRole))
+            return abort(403,__('Unauthorized'));
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,40 +5,33 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AdministratorsController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ApiController;
+use App\Http\Middleware\Role;
 
 
 
 
+Route::middleware(['auth','role:administrators'])->group(function(){
+    Route::get('/administrators', [AdministratorsController::class,'index'])-> name('administrators.index');
+    Route::post('/administrators', [AdministratorsController::class, 'store'])->name('administrators.store');
+    Route::delete('/administrators/{id}', [AdministratorsController::class, 'destroy'])->name('administrators.destroy');
+    Route::put('/administrators/{id}', [AdministratorsController::class, 'update'])->name('administrators.update');
+    Route::get('/administrators_edit/{id}', [AdministratorsController::class, 'edit'])->name('administrators.edit');
+});
 
-Route::get('/', [AdministratorsController::class,'index'])-> name('authors.index');
-Route::post('/authors', [AdministratorsController::class, 'store'])->name('authors.store');
-Route::delete('/authors/{id}', [AdministratorsController::class, 'destroy'])->name('authors.destroy');
-Route::put('/authors/{id}', [AdministratorsController::class, 'update'])->name('authors.update');
-Route::get('/authors_edit/{id}', [AdministratorsController::class, 'edit'])->name('authors.edit');
-
-
-// Route::get('/books', [CampusController::class, 'index'])->name('books.index');
-// Route::post('/books', [CampusController::class, 'store'])->name('books.store');
-// Route::delete('/books/{id}', [CampusController::class, 'destroy'])->name('books.destroy');
-// Route::put('/books/{id}', [CampusController::class, 'update'])->name('books.update');
-// Route::get('/books_edit/{id}', [CampusController::class, 'edit'])->name('books.edit');
-
-// Route::get('/', [SportsController::class,'index'])-> name('authors.index');
-// Route::post('/authors', [SportsController::class, 'store'])->name('authors.store');
-// Route::delete('/authors/{id}', [SportsController::class, 'destroy'])->name('authors.destroy');
-// Route::put('/authors/{id}', [SportsController::class, 'update'])->name('authors.update');
-// Route::get('/authors_edit/{id}', [SportsController::class, 'edit'])->name('authors.edit');
+Route::middleware(['auth','role:users'])->group(function(){
+    Route::get('/users', [UsersController::class, 'index'])->name('users.index');
+    Route::post('/users', [UsersController::class, 'store'])->name('users.store');
+    Route::delete('/users/{id}', [UsersController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{id}', [UsersController::class, 'update'])->name('users.update');
+    Route::get('/users_edit/{id}', [UsersController::class, 'edit'])->name('users.edit');
+});
 
 
-Route::get('/users', [UsersController::class, 'index'])->name('users.index');
-Route::post('/users', [UsersController::class, 'store'])->name('users.store');
-Route::delete('/users/{id}', [UsersController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{id}', [UsersController::class, 'update'])->name('users.update');
-Route::get('/users_edit/{id}', [UsersController::class, 'edit'])->name('users.edit');
+Route::middleware(['auth','role:obtener-datos'])->group(function(){
+    Route::get('/obtener-datos', [ApiController::class, 'obtenerDatos']);
+});
 
-
-
-Route::get('/obtener-datos', [ApiController::class, 'obtenerDatos']);
-
-Route::get('/get_users', [UserController::class, 'getUsers']);
+Route::middleware(['auth','role:get_users'])->group(function(){
+    Route::get('/get_users', [UserController::class, 'get_users']);
+});
 
